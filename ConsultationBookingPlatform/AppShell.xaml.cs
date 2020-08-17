e200f4e341ea61aa57f0c106e6fa418749e30717
Aydan Hardidge <env10us@outlook.com>
2020-08-17 14:21:06 +1000

    Initial Commit

---
+++ ConsultationBookingPlatform/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using ConsultationBookingPlatform.ViewModels;
+using ConsultationBookingPlatform.Views;
+using Xamarin.Forms;
+
+namespace ConsultationBookingPlatform
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ ConsultationBookingPlatform/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using ConsultationBookingPlatform.Models;
+using ConsultationBookingPlatform.ViewModels;
+
+namespace ConsultationBookingPlatform.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
