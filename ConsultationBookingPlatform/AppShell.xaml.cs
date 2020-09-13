c9eb2c784d51dd4da498a87591b0035f0f140834
gerrit11 <gerrit.oud@connect.qut.edu.au>
2020-09-13 15:14:52 +1000

    calander

--- ConsultationBookingPlatform/AppShell.xaml.cs
+++ ConsultationBookingPlatform/AppShell.xaml.cs
@@ -13,6 +13,7 @@ namespace ConsultationBookingPlatform
             InitializeComponent();
             Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
             Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+            Routing.RegisterRoute(nameof(CalanderPage), typeof(CalanderPage));
         }
 
         private async void OnMenuItemClicked(object sender, EventArgs e)
