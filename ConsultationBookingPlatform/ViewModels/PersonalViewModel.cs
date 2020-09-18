6d34fd7f7df257eb733aea07bb00d4e52e441492
gerrit11 <gerrit.oud@connect.qut.edu.au>
2020-09-18 16:45:41 +1000

    personal page xaml
    framwork only under personal tab

---
+++ ConsultationBookingPlatform/ViewModels/PersonalViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace ConsultationBookingPlatform.ViewModels
+{
+    class PersonalViewModel : BaseViewModel
+    {
+        public PersonalViewModel()
+        {
+            Title = "Personal information";
+
+        }
+    }
+}
